fe8bb9e7aaf0aedc04652e49a1af5f3f949b85a3
David Wu <vladylo98@gmail.com>
2025-08-27 10:09:54 +0800

    first commit

---
+++ src/main/java/com/tibame/order/model/OrderVO.java
@@ -0,0 +1,19 @@
+package com.tibame.order.model;
+
+public class OrderVO {
+	private Integer ordId;
+	private Integer memId;
+	public Integer getOrdId() {
+		return ordId;
+	}
+	public void setOrdId(Integer ordId) {
+		this.ordId = ordId;
+	}
+	public Integer getMemId() {
+		return memId;
+	}
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+	
+}
